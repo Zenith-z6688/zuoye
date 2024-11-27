12f1d610a84945cc762d1affbf2fba3a1f83db8a
Zenith-z6688 <1776523245@qq.com>
2024-11-27 22:55:33 +0800

    zuoye

---
+++ CMakeFiles/zuoye.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zuoye.dir/main.cpp.o"
+  "CMakeFiles/zuoye.dir/main.cpp.o.d"
+  "zuoye"
+  "zuoye.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zuoye.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
